7df2bb59ce82e7f41c6eaff8150482a737290df0
nika1145 <92970069+nika1145@users.noreply.github.com>
2021-12-21 19:09:02 +0400

    Add files via upload

---
+++ FragmentThird.kt
@@ -0,0 +1,29 @@
+package fragments
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.adapter.ViewPagerFragmentAdapter
+import com.example.davaleban6.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class FragmentThird: Fragment(R.layout.fragment_third) {
+//    private lateinit var editText: EditText
+//    private lateinit var buttonAdd: Button
+//    private lateinit var textView: TextView
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_third)
+//        editText = findViewById(R.id.editText)
+//        buttonAdd = findViewById(R.id.buttonAdd)
+//        textView = findViewById(R.id.textView)
+//        val note = editText.text.toString()
+//        val text = textView.text.toString()
+//        val result = note + "\n" + text
+//        textView.text = result
+//    }
+}
