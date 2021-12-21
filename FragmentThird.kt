2b30a1b7427075652ee574bb07449131e834cf5d
nika1145 <92970069+nika1145@users.noreply.github.com>
2021-12-21 19:14:47 +0400

    Update FragmentThird.kt

--- FragmentThird.kt
+++ FragmentThird.kt
@@ -12,18 +12,18 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class FragmentThird: Fragment(R.layout.fragment_third) {
-//    private lateinit var editText: EditText
-//    private lateinit var buttonAdd: Button
-//    private lateinit var textView: TextView
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.fragment_third)
-//        editText = findViewById(R.id.editText)
-//        buttonAdd = findViewById(R.id.buttonAdd)
-//        textView = findViewById(R.id.textView)
-//        val note = editText.text.toString()
-//        val text = textView.text.toString()
-//        val result = note + "\n" + text
-//        textView.text = result
-//    }
+    private lateinit var editText: EditText
+    private lateinit var buttonAdd: Button
+    private lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_third)
+        editText = findViewById(R.id.editText)
+        buttonAdd = findViewById(R.id.buttonAdd)
+        textView = findViewById(R.id.textView)
+        val note = editText.text.toString()
+        val text = textView.text.toString()
+        val result = note + "\n" + text
+        textView.text = result
+    }
 }
